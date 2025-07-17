d5465f1bd1ad0c07bcd34c674ef30989dde10241
methrick <methrickjoseraja2004@gmail.com>
2025-07-17 10:33:21 +0530

    message

---
+++ main.tf
@@ -0,0 +1,15 @@
+module "user" {
+    source = "./modules/iam"
+    sanodiuytr = "rajanaan"
+}
+
+# module "user1" {
+#     source = "./modules/s3"
+  
+# }
+
+# module "user2" {
+#     source = "./modules/ec2"
+  
+# }
+
